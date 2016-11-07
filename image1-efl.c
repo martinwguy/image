63cb744205041a128b2e6ff926ccfb8220a3da68
Martin Guy <martinwguy@gmail.com>
2016-11-07 21:46:48 +0100

    Reorder calls in image1-efl.c

--- image1-efl.c
+++ image1-efl.c
@@ -5,12 +5,18 @@
  *     Martin Guy <martinwguy@gmail.com>, October 2016.
  */
 #include <Elementary.h>
+#include <stdio.h>
  
 EAPI_MAIN int
 elm_main(int argc, char **argv)
 {
    Evas_Object *win;
    Evas_Object *image;
+
+   if (argc != 2) {
+	fprintf(stderr, "Usage: %s filename\n", argv[0]);
+	exit(1);
+   }
  
    elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);
  
@@ -21,16 +27,15 @@ elm_main(int argc, char **argv)
    elm_image_resizable_set(image, EINA_TRUE, EINA_TRUE);
    elm_image_aspect_fixed_set(image, EINA_FALSE);
    elm_image_file_set(image, "image.jpg", NULL);
-
-   elm_win_resize_object_add(win, image);
-   evas_object_show(image);
    {
       int w, h;
       elm_image_object_size_get(image, &w, &h);
       evas_object_resize(win, w, h);
    }
+   elm_win_resize_object_add(win, image);
+   evas_object_show(image);
    evas_object_show(win);
- 
+
    elm_run();
 
    return 0;
