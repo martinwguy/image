f6db6218250fa7418e8a9f82653d7bf1a028150c
Martin Guy <martinwguy@gmail.com>
2016-11-10 19:40:34 +0100

    Allow filename argument insead of default "image.jpg"

--- image1-efl.c
+++ image1-efl.c
@@ -5,19 +5,14 @@
  *     Martin Guy <martinwguy@gmail.com>, October 2016.
  */
 #include <Elementary.h>
-#include <stdio.h>
  
 EAPI_MAIN int
 elm_main(int argc, char **argv)
 {
    Evas_Object *win;
    Evas_Object *image;
+   char *filename = (argc > 1) ? argv[1] : "image.jpg";
 
-   if (argc != 2) {
-	fprintf(stderr, "Usage: %s filename\n", argv[0]);
-	exit(1);
-   }
- 
    elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);
  
    win = elm_win_util_standard_add("Image", "Image resizer");
@@ -26,7 +21,7 @@ elm_main(int argc, char **argv)
    image = elm_image_add(win);
    elm_image_resizable_set(image, EINA_TRUE, EINA_TRUE);
    elm_image_aspect_fixed_set(image, EINA_FALSE);
-   elm_image_file_set(image, "image.jpg", NULL);
+   elm_image_file_set(image, filename, NULL);
    {
       int w, h;
       elm_image_object_size_get(image, &w, &h);
@@ -40,4 +35,5 @@ elm_main(int argc, char **argv)
 
    return 0;
 }
+
 ELM_MAIN()
