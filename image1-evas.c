15e87d441cf2c7c1938276fa80f2a75beb6938de
Martin Guy <martinwguy@gmail.com>
2016-12-07 16:28:42 +0100

    Fix all bugs

--- image1-evas.c
+++ image1-evas.c
@@ -8,9 +8,9 @@
  * If they hit Control-Q or poke the [X] icon in the window's titlebar,
  * the application should quit.
  *
- * Bugs:
- *    -	Opens a window of the correct size but with black contents.
- *    - Doesn't react to Control-Q, maybe because the image is not present
+ * See http://docs.enlightenment.org/auto/Ecore_Evas_Window_Sizes_Example_c.html
+ *
+ * Bugs: None.
  *
  *     Martin Guy <martinwguy@gmail.com>, December 2016.
  */
@@ -24,7 +24,6 @@ int
 main(int argc, char **argv)
 {
     Ecore_Evas *ee;
-    Evas *canvas;
     Evas_Object *image;
     char *filename = (argc > 1) ? argv[1] : "image.jpg";
 
@@ -38,28 +37,32 @@ main(int argc, char **argv)
 	fputs("Can't initialise graphics system.\n", stderr);
 	exit(1);
     }
+    ecore_evas_callback_delete_request_set(ee, quitGUI);
+    ecore_evas_title_set(ee, "image1-evas");
     ecore_evas_show(ee);
-    canvas = ecore_evas_get(ee);
 #if 1
-    image = evas_object_image_filled_add(canvas);
+    {
+	Evas *canvas = ecore_evas_get(ee);
+	image = evas_object_image_filled_add(canvas);
+    }
 #else
     image = ecore_evas_object_image_new(ee);
     evas_object_image_filled_set(image, EINA_TRUE);
 #endif
     evas_object_image_file_set(image, filename, NULL);
     {
+        /* Set the window size to fit the image */
 	int w, h;
 	evas_object_image_size_get(image, &w, &h);
-	ecore_evas_resize(ecore_evas_object_ecore_evas_get(image), w, h);
 	ecore_evas_resize(ee, w, h);
     }
     evas_object_show(image);
 
-    //ecore_evas_callback_resize_set(ee, resize_cb);
-    ecore_evas_callback_delete_request_set(ee, quitGUI);
-    ecore_evas_callback_destroy_set(ee, quitGUI);
+    /* Propagate resize events from the container to the image */
+    ecore_evas_object_associate(ee, image, 0);
+
+    evas_object_focus_set(image, EINA_TRUE); // Without this, no keydown events
     evas_object_event_callback_add(image, EVAS_CALLBACK_KEY_DOWN, keyDown, NULL);
-    ecore_evas_title_set(ee, "image1-evas");
 
     ecore_main_loop_begin();
 
@@ -72,13 +75,14 @@ main(int argc, char **argv)
 static void
 keyDown(void *data, Evas *evas, Evas_Object *obj, void *einfo)
 {
-    const Evas_Modifier *mods;
     Evas_Event_Key_Down *ev = einfo;
+    const Evas_Modifier *mods = evas_key_modifier_get(evas);
 
-    mods = evas_key_modifier_get(evas);
+printf("Keydown\n");
+
     if (evas_key_modifier_is_set(mods, "Control") &&
 	strcmp(ev->key, "q") == 0) {
-	exit(0);	/* There has to be a more graceful way! */
+	ecore_main_loop_quit();
     }
 }
 
@@ -86,5 +90,5 @@ keyDown(void *data, Evas *evas, Evas_Object *obj, void *einfo)
 static void
 quitGUI(Ecore_Evas *ee EINA_UNUSED)
 {
-	ecore_main_loop_quit();
+    ecore_main_loop_quit();
 }
